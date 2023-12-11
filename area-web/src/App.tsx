34fec999616fee5e88a482aed0080ef2e7ef12c5
Andréas LE TANTER <andreas.le-tanter@epitech.eu>
2023-12-11 09:42:10 +0100

    feat: implement web text input (#56)
    * feat: base react with typescript template displaying only a blank page with playground in the center
    
    * feat: components folder with template for CTA one
    
    * feat: add props for CTA component
    
    * feat: component for creating a CTA working as expected with only text entry
    
    * feat: CTA component now takes a onClick parameter for the action, it doesnt takes icon too
    
    * fix: variables colors for each components was laggy due to tailwindcss, it now takes only one variables storing all the css for the colors or more
    
    * feat: add password type for the textinput
    
    * feat: add svg eye icon with password input
    
    * feat: the eye icon when asking for password input can appear closed or not with the current state
    
    * feat: click interaction for switching between show and hide
    
    * fix: merge conflicts with main branch

--- area-web/src/App.tsx
+++ area-web/src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import CTA from './Components/CTA';
+import TextInput from './Components/TextInput';
 
 function App() {
+  const [message, setMessage] = useState("Nothing");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  }
   return (
     <div className="App">
       <div className="App-header">
-        <CTA buttonText="Get started" buttonStyle="bg-[#34A853] text-[#FFFFF]" onClick={() => {alert("message")}} />
+        <div>
+          <TextInput placeholder="username" textInputStyle="bg-[#00000] border-[#D9D9D9] text-[#8E8E93] focus:border-[#34A853]" />
+        </div>
+        <div className='p-4'>
+          <TextInput isPassword={true} placeholder="password" textInputStyle="bg-[#00000] border-[#D9D9D9] text-[#8E8E93] focus:border-[#34A853]" onChange={handleChange} />
+        </div>
+        <div>
+          <CTA buttonText="Get started" buttonStyle="bg-[#34A853] text-[#FFFFF]" onClick={() => {alert("Password is: " + message)}} />
+        </div>
       </div>
     </div>
   );
